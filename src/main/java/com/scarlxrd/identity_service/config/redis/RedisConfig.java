1d5aba258a6e60b09905cfd760b9c2d5884dbe34
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-06-29 20:26:45 -0300

    fix(redis): configure password in connection factory

--- src/main/java/com/scarlxrd/identity_service/config/redis/RedisConfig.java
+++ src/main/java/com/scarlxrd/identity_service/config/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -15,10 +16,15 @@ public class RedisConfig {
     private String redisHost;
     @Value("${spring.data.redis.port:6379}")
     private int redisPort;
+    @Value("${spring.data.redis.password:redisPassword}")
+    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost,redisPort);
+        if (redisPassword != null && !redisPassword.isBlank()) {
+            config.setPassword(RedisPassword.of(redisPassword));
+        }
         return new LettuceConnectionFactory(config);
     }
     @Bean
